f389567ef8153211a30cec80f1d1462590188b73
Bhawana <bhawanaanasane@gmail.com>
2019-11-07 19:36:13 +0530

    New project

---
+++ ASPNETCoreWithAngular/Data/WorkoutContext.cs
@@ -0,0 +1,19 @@
+﻿using ASPNETCoreWithAngular.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreWithAngular.Data
+{
+    public class WorkoutContext : DbContext
+    {
+        public WorkoutContext(DbContextOptions<WorkoutContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WorkOut> Workout { get; set; }
+    }
+}
